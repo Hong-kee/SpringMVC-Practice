3b802aeac213d59c805ed4625a896eca1ab68997
Hong-kee <ghdehfdl123@gmail.com>
2021-12-05 21:02:09 +0900

    2021/12/05 Spring MVC Practice(Old Controller, InternalViewResolver)

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//[과거 방식의 controller] 현재 쓰이는 @Controller와는 다르다.
+//@Component를 통해 OldController 스프링 빈의 이름을 url 패턴 방식으로 설정
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
